8262c7bf72a83c36be5b78aa7f6c328d8830a06e
SerjoKo <serlo.ko@gmail.com>
2021-05-23 16:52:17 +0500

    Список сотрудников и действие контроллера

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,11 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { id = 1, Surname = "Третьяк", Name = "Александр",  Middlename = "Иавнович",  Age = 29 },
+            new Employee { id = 2, Surname = "Нестеренко", Name = "Сергей",  Middlename = "Геннадьевич", Age = 35 },
+            new Employee { id = 3, Surname = "Краус", Name = "Артур",  Middlename = "Артурович", Age = 30 },
+
+        };
         private readonly IConfiguration _Configuration; 
 
         public HomeController(IConfiguration Configuration)
@@ -24,6 +32,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content(_Configuration["Creatings"]);
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 }
