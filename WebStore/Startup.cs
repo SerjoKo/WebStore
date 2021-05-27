cc4ae1a980edfb5d517421dc68d1283ce89cb3a7
SerjoKo <serlo.ko@gmail.com>
2021-05-27 21:05:50 +0500

    wwwroot Стили crbgns

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -9,44 +9,6 @@ namespace WebStore
 {
     public class Startup
     {
-        //    public IConfiguration Configuration { get; }
-
-        //    public Startup(IConfiguration configuration)
-        //    {
-        //        Configuration = configuration;
-        //    }
-
-
-
-        //    // This method gets called by the runtime. Use this method to add services to the container.
-        //    public void ConfigureServices(IServiceCollection services)
-        //    {
-        //        services.AddRazorPages();
-        //    }
-
-        //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //    {
-        //        if (env.IsDevelopment())
-        //        {
-        //            app.UseDeveloperExceptionPage();
-        //        }
-        //        else
-        //        {
-        //            app.UseExceptionHandler("/Error");
-        //        }
-
-        //        app.UseStaticFiles();
-
-        //        app.UseRouting();
-
-        //        app.UseAuthorization();
-
-        //        app.UseEndpoints(endpoints =>
-        //        {
-        //            endpoints.MapRazorPages();
-        //        });
-        //    }
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration Configuration)
