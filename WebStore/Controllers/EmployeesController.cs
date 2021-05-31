8d755978f91437341994daefd62d7b1d7dd2e5a5
SerjoKo <serlo.ko@gmail.com>
2021-05-31 19:38:59 +0500

    Редактирование сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using WebStore.Models;
 using WebStore.Servicess.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {   
@@ -32,5 +33,51 @@ namespace WebStore.Controllers
         {
             return View(_EmployeesData.GetAll());
         }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _EmployeesData.Get(id);
+            
+            if (employee is null) return NotFound();
+
+            var view_model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                SurName = employee.SurName,
+                MiddleName = employee.MiddleName,
+                Age = employee.Age,
+            };
+            return View(view_model); 
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel Model)
+        {
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                Name = Model.Name,
+                SurName = Model.SurName,
+                MiddleName = Model.MiddleName,
+                Age = Model.Age,
+            };
+
+            _EmployeesData.Update(employee);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
     }
+
+    
 }
--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebStore.Models
+﻿namespace WebStore.ViewModels
 {
-    public class Employee
+    public class EmployeeViewModel
     {
         public int Id { get; set; }
-        
+
         public string Name { get; set; }
-       
+        
         public string SurName { get; set; }
         
         public string MiddleName { get; set; }
