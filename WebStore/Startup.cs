8147c20134771df77b873ed63c0a39d3b144199d
SerjoKo <serlo.ko@gmail.com>
2021-06-02 23:10:18 +0500

    Работа с контроллером сервисов

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
 using WebStore.Inftastructure.MidleWare;
 using WebStore.Servicess;
 using WebStore.Servicess.Interfaces;
@@ -21,12 +23,24 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddScoped<ITestService, TestService>();
+            //services.AddScoped<IPrinter, DebugPrinter>();
+
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+
+            //services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
+
+            //services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
+
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
+            //var test_service = services.GetRequiredService<ITestService>();
+
+            //test_service.Test();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,4 +70,43 @@ namespace WebStore
             });
         }
     }
+
+    //interface ITestService
+    //{
+    //    void Test();
+    //}
+
+    //class TestService : ITestService
+    //{
+    //    private IPrinter _Printer;
+
+    //    public TestService(IPrinter Printer)
+    //    {
+    //        _Printer = Printer;
+    //    }
+
+    //    public void Test()
+    //    {
+    //        _Printer.Printer("Запуск теста");
+    //        //Debug.WriteLine("Запуск теста");
+    //    }
+    //}
+
+    //interface IPrinter
+    //{
+    //    void Printer(string str);
+    //}
+
+    //class DebugPrinter : IPrinter
+    //{
+    //    public DebugPrinter()
+    //    {
+
+    //    }
+
+    //    public void Printer(string str)
+    //    {
+    //        Debug.WriteLine(str);
+    //    }
+    //}
 }
