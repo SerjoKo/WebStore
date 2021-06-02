8d9a28abe92841798541690a37765560af5471d7
SerjoKo <serlo.ko@gmail.com>
2021-06-03 00:30:30 +0500

    Базовые классы, интерфейсы

---
+++ WebStore.Domain/Entitys/Base/Entity.cs
@@ -0,0 +1,9 @@
+﻿using WebStore.Domain.Entitys.Base.Interfaces;
+
+namespace WebStore.Domain.Entitys.Base
+{
+    public abstract class Entity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entitys/Base/NamedEntity.cs
@@ -0,0 +1,9 @@
+﻿using WebStore.Domain.Entitys.Base.Interfaces;
+
+namespace WebStore.Domain.Entitys.Base
+{
+    class NamedEntity : Entity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
