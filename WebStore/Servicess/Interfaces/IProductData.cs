2a03ba7131be2d1b06bae8481336600c7d394e91
SerjoKo <serlo.ko@gmail.com>
2021-06-03 00:52:15 +0500

    Интерфейс сервиса каталога

---
+++ WebStore/Servicess/Interfaces/IProductData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entitys;
+
+namespace WebStore.Servicess.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
