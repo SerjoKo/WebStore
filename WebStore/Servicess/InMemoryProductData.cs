f1b6ab7bc0044e1b8adea57c528c3944d98f7e48
SerjoKo <serlo.ko@gmail.com>
2021-06-03 01:44:27 +0500

    Свойство с массивом секций

---
+++ WebStore/Servicess/InMemoryProductData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entitys;
+using WebStore.Servicess.Interfaces;
+
+namespace WebStore.Servicess
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
