0d1e9de57c25e9a7d1724d4141371fc70723735a
SerjoKo <serlo.ko@gmail.com>
2021-06-04 22:50:17 +0500

    Представление каталога товаров

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Servicess.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+
+        public IActionResult Index(int? SectionId, int? BrandId)
+        {
+            return View();
+        }
+    }
+}
