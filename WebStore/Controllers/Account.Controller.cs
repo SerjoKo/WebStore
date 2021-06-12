2d17b32afbc649585e6ade1d6078063f92dc5d74
SerjoKo <serlo.ko@gmail.com>
2021-06-13 00:44:52 +0500

    Логика регистрации пользователей

--- WebStore/Controllers/Account.Controller.cs
+++ WebStore/Controllers/Account.Controller.cs
@@ -1,10 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStore.Domain.Entitys.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View();
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var register_result = await _userManager.CreateAsync(user, Model.Password);
+
+            if (register_result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var identity_error in register_result.Errors)
+            {
+                ModelState.AddModelError("", identity_error.Description);
+            }
+
+            return View(Model);
+        }
 
         public IActionResult Login() => View();
 
