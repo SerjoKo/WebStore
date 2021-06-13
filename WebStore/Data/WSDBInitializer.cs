cf09515d9138fc88b4209146a707755e0c448d55
SerjoKo <serlo.ko@gmail.com>
2021-06-13 19:34:27 +0500

    Модернизация инициализатора

--- WebStore/Data/WSDBInitializer.cs
+++ WebStore/Data/WSDBInitializer.cs
@@ -53,48 +53,86 @@ namespace WebStore.Data
                 Logger.LogInformation("Инициализации таблицы товаров не требуется");
             }
 
+            #region Грохнул
             // Секции        
-            Logger.LogInformation("Инициализации таблицы секций");
+            //Logger.LogInformation("Инициализации таблицы секций");
+
+            //using (db.Database.BeginTransaction())
+            //{
+            //    db.Sections.AddRange(TestData.Sections);
+
+            //    db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Sections] ON");
+            //    db.SaveChanges();
+            //    db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+
+            //    db.Database.CommitTransaction();
+            //}
+
+            //Logger.LogInformation("Инициализации таблицы секций завершена");
+
+            //// Бренды        
+            //Logger.LogInformation("Инициализации таблицы брендов");
+
+            //using (db.Database.BeginTransaction())
+            //{
+            //    db.Brands.AddRange(TestData.Brands);
+
+            //    db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Brands] ON");
+            //    db.SaveChanges();
+            //    db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+
+            //    db.Database.CommitTransaction();
+            //}
+
+            //Logger.LogInformation("Инициализации таблицы брендов завершена");
+
+            //// Товары
+            //Logger.LogInformation("Инициализации таблицы товаров");
+            #endregion
+
+            var section_pool = TestData.Sections.ToDictionary(section => section.Id);
+            var brand_pool = TestData.Brands.ToDictionary(brand => brand.Id);
+
+            foreach (var section in TestData.Sections.Where(s => s.ParentId != null))
+            {
+                section.Parent = section_pool[(int)section.ParentId!];
+            }
+
+            foreach (var product in TestData.Products)
+            {
+                product.Section = section_pool[product.SectionId];
+                if (product.BrandId is { } brand_id)
+                {
+                    product.Brand = brand_pool[brand_id];
+                }
+
+                product.Id = 0;
+                product.SectionId = 0;
+                product.BrandId = null;
+            }
+
+            foreach (var section in TestData.Sections)
+            {
+                section.Id = 0;
+                section.ParentId = null;
+            }
+
+            foreach (var brand in TestData.Brands)
+            {
+                brand.Id = 0;
+
+            }
 
             using (db.Database.BeginTransaction())
             {
                 db.Sections.AddRange(TestData.Sections);
-
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Sections] ON");
-                db.SaveChanges();
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Sections] OFF");
-
-                db.Database.CommitTransaction();
-            }
-
-            Logger.LogInformation("Инициализации таблицы секций завершена");
-
-            // Бренды        
-            Logger.LogInformation("Инициализации таблицы брендов");
-
-            using (db.Database.BeginTransaction())
-            {
                 db.Brands.AddRange(TestData.Brands);
 
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Brands] ON");
-                db.SaveChanges();
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Brands] OFF");
-
-                db.Database.CommitTransaction();
-            }
-
-            Logger.LogInformation("Инициализации таблицы брендов завершена");
-
-            // Товары
-            Logger.LogInformation("Инициализации таблицы товаров");
-
-            using (db.Database.BeginTransaction())
-            {
                 db.Products.AddRange(TestData.Products);
 
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] ON");
+                //db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] ON");
                 db.SaveChanges();
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] OFF");
+                //db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] OFF");
 
                 db.Database.CommitTransaction();
             }
