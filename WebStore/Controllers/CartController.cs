faa2ba1f0afc8fdfc8493f3527f24fb46b5dd697
SerjoKo <serlo.ko@gmail.com>
2021-06-18 00:02:48 +0500

    Представление корзины

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Servicess.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService)
+        {
+            _CartService = CartService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_CartService.GetCartViewModel());
+        }
+    }
+}
